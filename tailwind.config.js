c7bc28ad76d444df8d61c3461bb7e61e8e86da95
AdibHoque <adibhoque04@gmail.com>
2024-05-09 21:07:04 +0600

    Initialization with Vite React, T

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+import DaisyUI from 'daisyui'
+export default {
+
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [DaisyUI],
+}
+
